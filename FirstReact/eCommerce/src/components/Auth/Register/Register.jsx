5850f9f0c0e61f37ffdfacfd1c0818f33619ac79
fathiaoyinloye <fathiaoyinloye20@gmail.com>
2026-02-22 13:39:56 +0100

    Register Page Finished

--- FirstReact/eCommerce/src/components/Auth/Register/Register.jsx
+++ FirstReact/eCommerce/src/components/Auth/Register/Register.jsx
@@ -1,44 +1,51 @@
 import React from "react";
+import styles from "../Register/register.module.css";
 
-const Register = () =>{
-    return (
-    
-     <div className={styles.loginPage}>
-          <div className={styles.name}>
-            OmoTemmy's
-            <p>E-Commerce</p>
-          </div>
-    
-          <div className={styles.todoBody}>
-            <div className={styles.container}>
-              <h1>Register</h1>
-              <form action="" className={styles.userDetails}>
-                <input
-                  type="text"
-                  placeholder="Enter your username"
-                  minlength="6"
-                  required
-                  id={styles.username}
-                />
-                <input
-                  type="password"
-                  placeholder="Enter your password"
-                  minLength="6"
-                  id= {styles.password}
-                />
-                <button>submit</button>
-              </form>
-            </div>
+const Register = () => {
+  return (
+    <div className={styles.loginPage}>
+      <div className={styles.name}>
+        OmoTemmy's
+        <p>E-Commerce</p>
+      </div>
+
+      <div className={styles.todoBody}>
+        <div className={styles.container}>
+          <h1>Register</h1>
+          <form action="" className={styles.userDetails}>
+            <input
+              type="text"
+              placeholder="Enter Your Name"
+              minlength="3"
+              required
+              id="name"
+            />
+            <input
+              type="text"
+              placeholder="Enter your username"
+              minlength="6"
+              required
+              id={styles.username}
+            />
+            <input
+              type="password"
+              placeholder="Enter your password"
+              minLength="6"
+              id={styles.password}
+            />
+            <button>submit</button>
+          </form>
+          <div className={styles.login}>
+            <h2>Already Have an Account</h2>
+            <button>login</button>
           </div>
         </div>
+      </div>
+    </div>
+  );
+};
 
-)
-}
-
-export default Register
-
-
-
+export default Register;
 
 // <!doctype html>
 // <html lang="en">
